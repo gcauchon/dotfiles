d1f16ba5083a3a022bcccf2498428c67d5684c6b
Guillaume <gcauchon@gmail.com>
2013-04-22 08:28:43 -0400

    minor changes to 'pwd' avec un dot-navigation alias is executed

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -5,10 +5,10 @@ alias ls='ls -G'
 alias ll='ls -laG'
 alias ld='ls -lG | grep "^d"'
 
-alias ..='cd ..'
-alias cd..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
+alias ..='cd .. && pwd'
+alias cd..='cd .. && pwd'
+alias ...='cd ../.. && pwd'
+alias ....='cd ../../.. && pwd'
 
 #mysql
 alias mysql_start='sudo /usr/local/mysql/support-files/mysql.server start'
