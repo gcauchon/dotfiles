2307e54082c843e39b66f0cb2557e9ddc980887a
Guillaume <gcauchon@gmail.com>
2014-03-05 21:37:11 -0500

    wakeonnas command removed from aliases since it's configure in my Alfred workflows now

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -9,6 +9,3 @@ alias ..='cd .. && pwd'
 alias cd..='cd .. && pwd'
 alias ...='cd ../.. && pwd'
 alias ....='cd ../../.. && pwd'
-
-#wakeonlan
-alias wake_nas='wakeonlan -i 192.168.1.255 2c:b0:5d:bf:07:b7'
